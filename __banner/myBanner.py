e33199dedef4951f9c8cea5794a8e34ef132c153
aamir <75577630+iamaamirkhan@users.noreply.github.com>
2021-06-06 06:54:47 +0000

    Add files via upload

---
+++ __banner/myBanner.py
@@ -0,0 +1,16 @@
+import colorama
+import random
+import sys
+
+def bannerTop():
+    banner = '''
+ 
+ Edu Mail Creator By Aamir 
+'''
+
+    bad_colors = ['BLACK', 'WHITE', 'LIGHTBLACK_EX', 'RESET']
+    codes = vars(colorama.Fore)
+    colors = [codes[color] for color in codes if color not in bad_colors]
+    colored_chars = [random.choice(colors) + char for char in banner]
+
+    return ''.join(colored_chars)
